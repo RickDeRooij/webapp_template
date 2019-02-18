3fb75db23bb611ca99b00b78c8313ba0cb00e438
RickDeRooij <47248418+RickDeRooij@users.noreply.github.com>
2019-02-18 13:48:18 +0100

    Add files via upload

--- gruntfile.js
+++ gruntfile.js
@@ -7,24 +7,23 @@ module.exports = function(grunt) {
                 separator: ';'
             },
             dist: {
-                src: ['javascript/*.js, style/*.css'],
-                dest: 'dist/'
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
             }
         },
         uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today() %> */\n'
+            },
             dist: {
                 files: {
                     'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
                 }
             }
         },
-        qunit: {
-            files: ['test/**/*.html']
-        },
         jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
-                // options here to override JSHint defaults
                 globals: {
                     jQuery: true
                 }
@@ -32,19 +31,27 @@ module.exports = function(grunt) {
         },
         watch: {
             files: ['<%= jshint.files %>'],
-            tasks: ['jshint', 'qunit']
+            tasks: ['jshint']
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/output.css': ['src/**/*.css', 'src/**/*.css']
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('test', ['jshint', 'qunit']);
-
-    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
 
 };
-
